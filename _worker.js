950088f9f3fcbe94b4a9bad9686482eb59b446fc
minjuangel <angelminju123@gmail.com>
2025-05-28 06:06:24 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,8 +3,8 @@ import { connect } from "cloudflare:sockets";
 // import { Buffer } from "node:buffer";
 
 // Variables
-const rootDomain = "pages.dev"; // Ganti dengan domain utama kalian
-const serviceName = "vpnlife"; // Ganti dengan nama workers kalian
+const rootDomain = "huixuan.my.id"; // Ganti dengan domain utama kalian
+const serviceName = "vpn"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
